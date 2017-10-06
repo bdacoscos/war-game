320cc5dd4fd44713f1454813a967dc6b279fb8ee
Briana Dacoscos <briana.dacoscos@gmail.com>
2017-10-06 14:44:15 -0700

    render card on page

---
+++ README.md
@@ -0,0 +1,34 @@
+WAR!
+
+When page loads, “main” game screen is displayed.
+	- Displays:
+		- Title of game
+		- Game play description
+		- 2 decks of cards
+		- DEAL button
+
+Player clicks DEAL button
+	- Card counters display (start at 26 each)
+	- Two cards are “drawn”
+		- Higher card between the two wins
+	- Message to display winner (or war!) appears below
+	- Total number of cards drawn (2 in this case) are added to winner’s card counter
+
+If a tie occurs, it’s WAR!
+	- DEAL button starts to pulse
+	- DEAL button is clicked
+		- 3 cards are drawn and displayed face-down
+		- 4th card is drawn and displayed facing up
+		- Higher card wins
+			- Total number of cards drawn are added to winner’s counter
+
+Double-War, Triple War… can occur
+	- Cards can be drawn as long as a player has enough cards
+		- If player does not have enough cards to play WAR, can use last card available
+
+When player loses all cards
+	- Opponent wins
+	- Winner display message appears
+	- Reset button appears (Play again?)
+
+
--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
 $(function() {
-
 /*----- constants -----*/
 
-
-
+// on click, deal cards, determine winner or war, update vars, declare winner or war
+  // if winner: on next click, add all played cards to winner deck, deal cards
+  // if war: draw 4 cards, determine winner or war, update vars, declare winner or war
+    // if winner: on next click, add all played cards to winner deck, deal cards
+    // if war: draw 4 cards, determine winner or war, update vars, declare winner or war
+      // ...
 
 /*----- app's state (variables) -----*/
 var deck = [];
+var p1cards = [];
+var p2cards = [];
 var winner;
-var playerOne;
-var playerTwo;
-var shuffled = [];
 
 
 /*----- cached element references -----*/
 
 
-
-
 /*----- event listeners -----*/
 
-
+// event listener on PLAY button
+// event listener on DEAL button
+// event listener on WAR button
 
 
 /*----- functions -----*/
@@ -30,22 +32,22 @@ function init() {
   buildDeck();
   shuffle();
 
-
 }
 
 function buildDeck() {
   console.log('buildDeck is running');
   var suits = ['d', 'c', 'h', 's'];
-  var faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  var lookup = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14};
+  var faces = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+  var lookup = {'02': 2, '03': 3, '04': 4, '05': 5, '06': 6, '07': 7, '08': 8, '09': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14};
 
   suits.forEach(function(suit) {
     faces.forEach(function(face) {
       var card = {
-        // css: face + suit,
+        css: suit + face,
         value: lookup[face] 
       };
       deck.push(card);
+      
     })
   })
 }
@@ -53,23 +55,25 @@ function buildDeck() {
 function shuffle() {
   console.log('shuffling...');
   while (deck.length) {
-    shuffled.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
+    p1cards.push(deck.splice(Math.floor(Math.random() * deck.length), 1)[0]);
   }
-  console.log(shuffled);
+  console.log(p1cards);
+
+  
+
+  render();
 }
 
+
+
 // function checkForWinner() {
-
-
 // }
 
+function render() {
+  console.log('rendering...')
+  $('#playerOne').addClass('card').addClass(p1cards[0].css);
 
-// function render() {
-
-
-// }
+}
 
 init();
-// render();
-
 });
